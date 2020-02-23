9156e56d2bb6f25c244648efc7c1cba5729890de
Carl Lee <carllee@levo.com.au>
2020-02-23 14:05:48 +1100

    setup plop js

---
+++ plopfile.js
@@ -0,0 +1,33 @@
+module.exports = function (plop) {
+	// controller generator
+	plop.setGenerator('post', {
+		description: 'page template `post` logic',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'post file html name please'
+			},
+			{
+				type: 'input',
+				name: 'date',
+				message: 'date please (April 25, 2017)'
+			},
+			{
+				type: 'input',
+				name: 'title',
+				message: 'specify blog title'
+			},
+			{
+				type: 'input',
+				name: 'des',
+				message: 'des please'
+			},
+		],
+		actions: [{
+			type: 'add',
+			path: 'post/{{name}}.html',
+			templateFile: 'plop-templates/post.hbs'
+		}]
+	});
+};
